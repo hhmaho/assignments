f8bc4ec486f1f50dd762e96303f11feeeb723f8e
Marianne Hoffmann <hhmaho@gmail.com>
2018-11-20 09:50:59 +0100

    2 components start

---
+++ assignment-one/src/components/UserInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userInput = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>here comes an input element.</p>
+    </div>
+  );
+};
+
+export default userInput;
---
+++ assignment-one/src/components/UserOutput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const userOutput = props => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello, this is the first paragragraph in my useroutputcomponent.</p>
+      <p>Hello this is the second paragragraph in my useroutputcomponent.</p>
+    </div>
+  );
+};
+
+export default userOutput;
